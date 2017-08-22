bd772f4d2a6be15af99ce79103635d0f4f7fa7ea
Marat Agliulin <marat@176x214x244x5.dynamic.tmn.ertelecom.ru>
2017-08-22 18:05:10 +0500

    first draft

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.post('/sendresult', function(req, res) {
     console.log("Received new test result: " + results);
     fs.readFile('public/assets/results.txt', (err, data) => {
       if (err) console.log(err);
-      fs.writeFile('public/assets/results.txt', data + '\n' + req.ip + ': ' + results)
+      fs.writeFile('public/assets/results.txt', data + '\n' + req.headers['x-forwarded-for'] + ': ' + results)
     });
 
     // setup email data with unicode symbols
