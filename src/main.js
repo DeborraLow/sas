5e86ad270acc282f19fc3911d6a707d80075bee3
Marat Agliulin <marat@176x214x244x5.dynamic.tmn.ertelecom.ru>
2017-08-22 16:03:03 +0500

    first draft

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 import scss from './main.scss';
 //import js from './evercookies.js'
+var checkedItems = [];
 var startButton = document.getElementById('start-button');
 startButton.addEventListener('click', () => {
   document.getElementById('hello-screen').style.display = 'none';
   document.getElementById('test-screen-1').style.display = 'flex';
 })
-var  checkedItems = [];
 var listItems1 = document.querySelectorAll('#test-screen-1 .list-item');
 for (var i = 0; i < listItems1.length; i++) {
   listItems1[i].onclick = function() {
     if (document.getElementsByClassName('list-item_selected1').length < 2 ){
       this.classList.toggle('list-item_selected1');
-      checkedItems.push(this.id);
+      checkedItems.push(parseInt(this.id));
       if (document.getElementsByClassName('list-item_selected1').length  == 2){
-        document.getElementById('test-screen-1').style.display = 'none';
-        document.getElementById('test-screen-2').style.display = 'flex';
+        setTimeout(()=>{  document.getElementById('test-screen-1').style.display = 'none';
+          document.getElementById('test-screen-2').style.display = 'flex';},300)
+
       }
     }
   }
@@ -24,10 +25,11 @@ for (var i = 0; i < listItems2.length; i++) {
   listItems2[i].onclick = function() {
     if (document.getElementsByClassName('list-item_selected2').length < 2 ){
       this.classList.toggle('list-item_selected2');
-      checkedItems.push(this.id);
+      checkedItems.push(parseInt(this.id));
       if (document.getElementsByClassName('list-item_selected2').length  == 2){
-        document.getElementById('test-screen-2').style.display = 'none';
-        document.getElementById('test-screen-3').style.display = 'flex';
+        setTimeout(()=>{document.getElementById('test-screen-2').style.display = 'none';
+        document.getElementById('test-screen-3').style.display = 'flex';},300)
+
       }
     }
   }
@@ -38,10 +40,12 @@ for (var i = 0; i < listItems3.length; i++) {
   listItems3[i].onclick = function() {
     if (document.getElementsByClassName('list-item_selected3').length < 2 ){
       this.classList.toggle('list-item_selected3');
-      checkedItems.push(this.id);
+      checkedItems.push(parseInt(this.id));
       if (document.getElementsByClassName('list-item_selected3').length  == 2){
-        document.getElementById('test-screen-3').style.display = 'none';
+        setTimeout(()=>{document.getElementById('test-screen-3').style.display = 'none';
         document.getElementById('test-results').style.display = 'flex';
+        console.log(checkedItems);},300)
+
       }
     }
   }
