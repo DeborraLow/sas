5c58eefb116c7af900229c3e9ce99d41e467ce1a
Marat Agliulin <marat@176x214x14x122.dynamic.tmn.ertelecom.ru>
2017-08-24 00:32:07 +0500

    fd

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
 
   plugins: [
     new WebpackShellPlugin({
-  onBuildEnd: ['purifycss ' + path.resolve(__dirname, 'public') + '/main.css' + ' ' + path.resolve(__dirname, 'public') + '/index.html -o . -i -r']
+  onBuildEnd: ['purifycss ' + path.resolve(__dirname, 'public') + '/main.css' + ' ' + path.resolve(__dirname, 'public') + '/index.html -o ./main.css -i']
 }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
