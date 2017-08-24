2ec1f53855d893bfbd52df4ea6b190d06ff495d3
Marat Agliulin <marat@5x3x182x108.dynamic.omsk.ertelecom.ru>
2017-08-24 18:46:46 +0500

    fd

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import scss from './main.scss';
+import css from './javascript.fullPage.css'
+import js from './javascript.fullPage.js'
 //import js from './evercookies.js'
 var checkedItems = [];
 var progress = document.getElementById('progress');
@@ -6,6 +8,30 @@ var progressBar = document.getElementById('progress__bar');
 var background = document.getElementsByClassName('background')[0];
 var screenFilter = document.getElementsByClassName('screen__filter')[0];
 var startButton = document.getElementById('start-button');
+var lastScrollTop = 0;
+
+initialize('#fullpage', {
+  anchors: [
+    'firstPage', 'secondPage'
+  ],
+  menu: '#menu',
+  css3: false
+});
+function showButton(button){
+  if (location.hash === "#secondPage") {
+    window.setTimeout(() => {
+      button.classList.add('button_visible');
+      button.classList.remove('button_hidden');
+    }, 2000)
+}};
+var buttonStart = document.getElementById('start-button');
+showButton(buttonStart);
+if ("onhashchange" in window) {
+  window.onhashchange = ()=>{showButton(buttonStart);}
+} else {
+  showButton(buttonStart);
+}
+
 startButton.addEventListener('click', () => {
   document.getElementById('hello-screen').style.display = 'none';
   document.getElementById('test-screen-1').style.display = 'flex';
@@ -16,17 +42,17 @@ startButton.addEventListener('click', () => {
 var listItems1 = document.querySelectorAll('#test-screen-1 .list-item');
 for (var i = 0; i < listItems1.length; i++) {
   listItems1[i].onclick = function() {
-    if (document.getElementsByClassName('list-item_selected1').length < 2 ){
+    if (document.getElementsByClassName('list-item_selected1').length < 2) {
       this.classList.toggle('list-item_selected1');
       checkedItems.push(parseInt(this.id));
-      if (document.getElementsByClassName('list-item_selected1').length  == 2){
-        setTimeout(()=>{
+      if (document.getElementsByClassName('list-item_selected1').length == 2) {
+        setTimeout(() => {
           document.getElementById('test-screen-1').style.display = 'none';
           document.getElementById('test-screen-2').style.display = 'flex';
           background.style.minHeight = documentHeight() + 'px';
           screenFilter.style.minHeight = documentHeight() + 'px';
           progressBar.style.width = '33.3%';
-        },300)
+        }, 300)
 
       }
     }
@@ -35,12 +61,14 @@ for (var i = 0; i < listItems1.length; i++) {
 var listItems2 = document.querySelectorAll('#test-screen-2 .list-item');
 for (var i = 0; i < listItems2.length; i++) {
   listItems2[i].onclick = function() {
-    if (document.getElementsByClassName('list-item_selected2').length < 2 ){
+    if (document.getElementsByClassName('list-item_selected2').length < 2) {
       this.classList.toggle('list-item_selected2');
       checkedItems.push(parseInt(this.id));
-      if (document.getElementsByClassName('list-item_selected2').length  == 2){
-        setTimeout(()=>{document.getElementById('test-screen-2').style.display = 'none';
-        document.getElementById('test-screen-3').style.display = 'flex';},300)
+      if (document.getElementsByClassName('list-item_selected2').length == 2) {
+        setTimeout(() => {
+          document.getElementById('test-screen-2').style.display = 'none';
+          document.getElementById('test-screen-3').style.display = 'flex';
+        }, 300)
         background.style.minHeight = documentHeight() + 'px';
         screenFilter.style.minHeight = documentHeight() + 'px';
         progressBar.style.width = '66.6%';
@@ -52,38 +80,36 @@ for (var i = 0; i < listItems2.length; i++) {
 var listItems3 = document.querySelectorAll('#test-screen-3 .list-item');
 for (var i = 0; i < listItems3.length; i++) {
   listItems3[i].onclick = function() {
-    if (document.getElementsByClassName('list-item_selected3').length < 2 ){
+    if (document.getElementsByClassName('list-item_selected3').length < 2) {
       this.classList.toggle('list-item_selected3');
       checkedItems.push(parseInt(this.id));
-      if (document.getElementsByClassName('list-item_selected3').length  == 2){
+      if (document.getElementsByClassName('list-item_selected3').length == 2) {
         progressBar.style.width = '100%';
-        setTimeout(()=>{progress.style.display = 'none'},1500);
-        setTimeout(()=>{document.getElementById('test-screen-3').style.display = 'none';
-        document.getElementById('test-results').style.display = 'block';
-        background.style.minHeight = document.documentElement.clientHeight + 'px';
-        screenFilter.style.minHeight = document.documentElement.clientHeight + 'px';
-        background.style.minHeight = documentHeight() + 'px';
-        screenFilter.style.minHeight = documentHeight() + 'px';
-        sendResult(checkedItems);},300)
+        setTimeout(() => {
+          progress.style.display = 'none'
+        }, 1500);
+        setTimeout(() => {
+          document.getElementById('test-screen-3').style.display = 'none';
+          document.getElementById('test-results').style.display = 'block';
+          background.style.minHeight = document.documentElement.clientHeight + 'px';
+          screenFilter.style.minHeight = document.documentElement.clientHeight + 'px';
+          background.style.minHeight = documentHeight() + 'px';
+          screenFilter.style.minHeight = documentHeight() + 'px';
+          sendResult(checkedItems);
+        }, 300)
       }
     }
   }
 }
 
-function sendResult(items){
+function sendResult(items) {
   console.log(items);
-  var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
+  var xmlhttp = new XMLHttpRequest(); // new HttpRequest instance
   xmlhttp.open("POST", "/sendresult");
   xmlhttp.setRequestHeader("Content-Type", "application/json");
   xmlhttp.send(JSON.stringify(items));
 }
 
 function documentHeight() {
-    return Math.max(
-      document.documentElement.clientHeight,
-      document.body.scrollHeight,
-      document.documentElement.scrollHeight,
-      document.body.offsetHeight,
-      document.documentElement.offsetHeight
-    );
+  return Math.max(document.documentElement.clientHeight, document.body.scrollHeight, document.documentElement.scrollHeight, document.body.offsetHeight, document.documentElement.offsetHeight);
 }
