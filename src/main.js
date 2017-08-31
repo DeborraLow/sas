277eb9e361340439e34da1a2da3ade1f66c2b70e
Marat Agliulin <marat@Marats-MacBook-Pro.local>
2017-08-31 13:47:21 +0500

    sdfs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import scss from './main.scss';
-// import css from './javascript.fullPage.css'
-// import js from './javascript.fullPage.js'
+import css from './javascript.fullPage.css'
+import js from './javascript.fullPage.js'
 //import js from './evercookies.js'
 var checkedItems = [];
 var progress = document.getElementById('progress');
@@ -10,16 +10,19 @@ var screenFilter = document.getElementsByClassName('screen__filter')[0];
 var startButton = document.getElementById('start-button');
 var endButton = document.getElementById('end-button');
 var lastScrollTop = 0;
+var isLarge = true;
+if(window.innerWidth<=1000){isLarge = false};
 
 window.onload = ()=>{
   window.setTimeout(()=>{document.getElementsByClassName('h1_hello')[0].style.opacity = '1';
-  var results = document.getElementById('test-results');
-  results.style.backgroundImage = "url('assets/giphy.gif')";
-  results.style.backgroundSize = "cover";
-  results.style.display = "flex";
+
   document.getElementById('')
 },2000);
-window.setTimeout(()=>{fullpage.moveSectionDown();
+window.setTimeout(()=>{
+  var results = document.getElementById('test-results');
+  if (isLarge){results.style.backgroundImage = "url('assets/giphy.gif')";
+  results.style.backgroundSize = "cover";}
+  // results.style.display = "flex";
 },7000);
 
 // document.getElementById('fullpage').fullpage({
@@ -28,18 +31,23 @@ window.setTimeout(()=>{fullpage.moveSectionDown();
 //     ],
 //   responsiveWidth: 1000
 // });
-initialize('#fullpage', {
-  responsiveWidth: 1000,
-  anchors: [
-    'start', 'presentation','test1','test2','test3'
-  ]
-});
+if (isLarge){
+  initialize('#fullpage', {
+    anchors: [
+      'start', 'presentation','test1','test2','test3'
+    ]
+  });
+}
+else {
+  document.querySelector('#test2 .h2').style.display = 'none';
+  document.querySelector('#test3 .h2').style.display = 'none';
+}
 }
 
 
 
 function showButton(button) {
-  if (location.hash === "#presentation") {
+  if (location.hash === "#presentation" || !isLarge) {
     progress.style.display = 'none';
     progressBar.style.display = 'none';
     window.setTimeout(() => {
@@ -49,17 +57,17 @@ function showButton(button) {
   }
 };
 function progressBarHandler(){
-  if (location.hash === "#test1") {
+  if (location.hash === "#test1" && isLarge) {
     progress.style.display = 'block';
     progressBar.style.display = 'block';
     progressBar.style.width = '33.3%';
   }
-  else if (location.hash === "#test2") {
+  else if (location.hash === "#test2" && isLarge) {
     progress.style.display = 'block';
     progressBar.style.display = 'block';
     progressBar.style.width = '66.6%';
   }
-  else if (location.hash === "#test3") {
+  else if (location.hash === "#test3" && isLarge) {
     progress.style.display = 'block';
     progressBar.style.display = 'block';
     progressBar.style.width = '100%';
@@ -99,12 +107,12 @@ if ("onhashchange" in window) {
 }
 
 startButton.addEventListener('click', () => {
-  progress.style.display = 'block';
-  progressBar.style.display = 'block';
+  if (isLarge){progress.style.display = 'block';
+  progressBar.style.display = 'block';}
+  document.getElementById('test1').style.display = 'flex';
+  document.getElementById('test2').style.display = 'flex';
+  document.getElementById('test3').style.display = 'flex';
   window.location.href = '#test1';
-  fullpage.moveSectionDown();
-  // document.getElementById('presentation-screen').classList.remove('activeSection')
-  // document.getElementById('test-screen-1').classList.add('activeSection');
   // background.style.minHeight = documentHeight() + 'px';
   // screenFilter.style.minHeight = documentHeight() + 'px';
 })
@@ -118,7 +126,7 @@ for (var i = 0; i < listItems.length; i++) {
     else {
       checkedItems.push(parseInt(this.id));
     }
-    if(checkedItems.length>=3){
+    if(checkedItems.length>=0){
       endButton.classList.remove('button_disabled');
     }
     else {
@@ -130,14 +138,28 @@ for (var i = 0; i < listItems.length; i++) {
 endButton.addEventListener('click',()=>{
    if(!endButton.classList.contains('button_disabled')){
      sendResult(checkedItems);
-     window.location.href = '#test-results'
-     fullpage.moveSectionDown();
+     document.getElementById('test-results').style.display = 'block';
+     window.location.href = '#test-results';
+     if (isLarge) {fullpage.moveSectionDown();}
      document.getElementById('test-results').style.visibility = 'visible';
     //  fullpage.destroy('all');
     //  document.getElementById('test-screen-3').classList.remove('activeSection')
     //  document.getElementById('test-results').classList.add('activeSection');
    }
    else {alert("Выбери не менее 3 вариантов")}
+})
+
+var agreement = document.getElementById('agreement-checkbox');
+var submitButton = document.getElementById('form-subscribe__submit');
+if (agreement.checked){
+  submitButton.disabled = false;
+  submitButton.classList.remove('button_disabled');
+};
+agreement.addEventListener('click', ()=>{
+  if (agreement.checked){
+    submitButton.disabled = false;
+    submitButton.classList.remove('button_disabled');
+  }
 })
 
 function sendResult(items) {
