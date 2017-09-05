93226182f02cb14bd5e69d8e37ffcd27d33e34fc
Marat Agliulin <marat@176x214x255x116.dynamic.tmn.ertelecom.ru>
2017-09-05 12:25:51 +0500

    fd

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(xFrameOptions('ALLOW-FROM http://webvisor.com/'));
+// app.use(xFrameOptions('ALLOW-FROM http://webvisor.com/'));
 // app.use(evercookie.backend({
 //   pngPath: '/evercookie_png.php',
 //   etagPath: '/evercookie_etag.php',
