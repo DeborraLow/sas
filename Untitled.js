21b1439fca2b2495662abf95cda23aea11566e2e
Marat Agliulin <marat@net161.255.92-141.tmn.ertelecom.ru>
2017-09-08 17:40:33 +0500

    fd

--- Untitled.js
+++ Untitled.js
@@ -2,27 +2,41 @@ function canUseWebP() {
   var e = document.createElement("canvas");
   return 0 == e.toDataURL("image/webp").indexOf("data:image/webp")
 }
+var sheet = (function(){
+  var style = document.createElement("style");
+  style.appendChild(document.createTextNode(""));
+  document.head.appendChild(style);
+  return style.sheet;
+})();
+function addCSSRule(sheet, selector, rules, index) {
+	if("insertRule" in sheet) {
+		sheet.insertRule(selector + "{" + rules + "}", index);
+	}
+	else if("addRule" in sheet) {
+		sheet.addRule(selector, rules, index);
+	}
+}
 dummyImg = document.createElement("img"),
 canUseWebP()
   ? window.innerWidth > 800
     ? (dummyImg.src = "assets/blackboard.webp", dummyImg.onload = function() {
-      document.head.appendChild(document.createElement("style").appendChild(document.createTextNode('.hello-screen::before{background:url("assets/blackboard.webp"); background-size:cover}'))),
+      addCSSRule(document.styleSheets[0], ".hello-screen::before", "background:url('assets/blackboard.webp'); background-size:cover");
       document.getElementById("load").style.visibility = "hidden",
       yaCounter45804912.reachGoal("1_SCREEN_VISITED")
     })
     : (dummyImg.src = "assets/blackboard.jpg", dummyImg.onload = function() {
-      document.head.appendChild(document.createElement("style").appendChild(document.createTextNode('.hello-screen::before{background:url("assets/blackboard.jpg"); background-size:cover}'))),
+      addCSSRule(document.styleSheets[0], ".hello-screen::before", "background:url('assets/blackboard.jpg'); background-size:cover");
       document.getElementById("load").style.visibility = "hidden",
       yaCounter45804912.reachGoal("1_SCREEN_VISITED")
     })
   : window.innerWidth > 800
     ? (dummyImg.src = "assets/blackboard.gif", dummyImg.onload = function() {
-      document.head.appendChild(document.createElement("style").appendChild(document.createTextNode('.hello-screen::before{background:url("assets/blackboard.gif"); background-size:cover}'))),
+      addCSSRule(document.styleSheets[0], ".hello-screen::before", "background:url('assets/blackboard.gif'); background-size:cover");
       document.getElementById("load").style.visibility = "hidden",
       yaCounter45804912.reachGoal("1_SCREEN_VISITED")
     })
     : (dummyImg.src = "assets/blackboard.jpg", dummyImg.onload = function() {
-      document.head.appendChild(document.createElement("style").appendChild(document.createTextNode('.hello-screen::before{background:url("assets/blackboard.jpg"); background-size:cover}'))),
+      addCSSRule(document.styleSheets[0], ".hello-screen::before", "background:url('assets/blackboard.jpg'); background-size:cover");
       document.getElementById("load").style.visibility = "hidden",
       yaCounter45804912.reachGoal("1_SCREEN_VISITED")
     });
